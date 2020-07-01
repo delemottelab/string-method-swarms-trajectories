beafe710a2ba5ff6cfd3c814e2e5d50b87e7faa7
oliver <oliver.fleetwood@scila.se>
2020-07-01 13:47:04 +0200

    Visualizing dihedrals with d-PCA

--- examples/alanine-dipeptide/visualize_free_energy_adp.py
+++ examples/alanine-dipeptide/visualize_free_energy_adp.py
@@ -6,9 +6,10 @@ import numpy as np
 sys.path.append("../../../")
 from stringmethod.config import *
 from stringmethod.postprocessing import *
+from matplotlib import ticker, colors
 
 
-def show(grid, free_energy, string, fe_cut_off=10):
+def show(grid: np.array, free_energy: np.array, string: np.array = None, fe_cut_off: float = 10):
     free_energy[free_energy > fe_cut_off] = np.nan
     phi = grid[:, 0]
     if free_energy.shape[1] == 1:
@@ -20,36 +21,53 @@ def show(grid, free_energy, string, fe_cut_off=10):
                           psi,
                           free_energy.T,  # TODO should we take the transform of the matrix or not?
                           levels=20,
+                          norm=colors.PowerNorm(gamma=1 / 3),
                           cmap=plt.cm.rainbow)
         cbar = plt.colorbar(im)
         cbar.set_label("[kcal/mol]")
         if string is not None:
             plt.plot(string[:, 0], string[:, 1], '--o', label="String")
             plt.legend()
-        plt.ylabel("$\psi$")
-    plt.xlabel("$\phi$")
     plt.tight_layout()
     plt.savefig("free_energy.png")
     plt.show()
 
 
+def handle_periodicity(cv_coordinates: np.array, dpca: bool = True):
+    """
+    # Handle periodic boundary conditions by taking modules 2pi or performing dihedral-PCA
+    :param dpca: if True, perform dPCA onto the input and convert it into two components
+    :param cv_coordinates:
+    :return:
+    """
+
+    offset = -np.pi / 4  # you may play around with an offset to make the plot more continuous
+    cv_coordinates = offset + cv_coordinates % (2 * np.pi)
+
+    if dpca:
+        from sklearn.decomposition import PCA
+        new_coords = np.empty((len(cv_coordinates), cv_coordinates.shape[1], 4))
+        new_coords[:, :, 0] = np.sin(cv_coordinates[:, :, 0])
+        new_coords[:, :, 1] = np.cos(cv_coordinates[:, :, 0])
+        new_coords[:, :, 2] = np.sin(cv_coordinates[:, :, 1])
+        new_coords[:, :, 3] = np.cos(cv_coordinates[:, :, 1])
+        pca = PCA(n_components=2)
+        pca.fit(new_coords.reshape((2 * len(new_coords), 4)))
+        for idx, nc in enumerate(new_coords):
+            cv_coordinates[idx] = pca.transform(nc)
+
+    return cv_coordinates
+
+
 def compute():
     config = load_config("config.json")
     cv_coordinates = np.load("postprocessing/cv_coordinates.npy")
-    string = np.loadtxt("strings/string0.txt")
-    # Handle periodic boundary conditions
-    # Two options are shown below. Using modulus or taking the sine value
-    offset = -np.pi / 4  # you may play around with an offset to make the plot more continuous
-    cv_coordinates = offset + cv_coordinates % (2 * np.pi)
-    string = offset + string % (2 * np.pi)
-    # Note that simply taking the sine value reduces dimensionality. Use with care
-    # cv_coordinates = np.sin(cv_coordinates)
-    # string = np.sin(string)
+    cv_coordinates = handle_periodicity(cv_coordinates)
 
-    # Select only phi
+    # Uncomment to only select one CV
     # cv_coordinates = cv_coordinates[:, :, 0]
     tc = TransitionCountCalculator(config=config,
-                                   n_grid_points=10,
+                                   n_grid_points=13,
                                    cv_coordinates=cv_coordinates)
     tc.run()
     tc.persist()
@@ -58,9 +76,9 @@ def compute():
                               transition_count=tc.transition_count)
     fc.run()
     fc.persist()
-    return tc.grid, fc.free_energy, string
+    return tc.grid, fc.free_energy
 
 
 if __name__ == "__main__":
-    grid, free_energy, string = compute()
-    show(grid, free_energy, string)
+    grid, free_energy = compute()
+    show(grid, free_energy)
