f21a8d2ae4aef585473a4221f4c7817f5d746b1a
sperezconesa <sperezconesa@gmail.com>
2021-06-30 18:29:29 +0200

    Removed slave_master architecture from gmx_jobs.py

--- gmx_jobs/mpi_master_slave/__init__.py
+++
@@ -1,4 +0,0 @@
-from gmx_jobs.mpi_master_slave.master_slave import *
-from gmx_jobs.mpi_master_slave.work_queue import *
-from gmx_jobs.mpi_master_slave.multi_work_queue import *
-import gmx_jobs.mpi_master_slave.exceptions
